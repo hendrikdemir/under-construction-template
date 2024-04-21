f7dd1ed75808e98704bcf439e27199a12f9dd002
Hendrik Demir <h.c.s.demir@demirsolutions.nl>
2024-04-21 15:54:06 +0200

    New version

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style.css';
+import constructionImage from './construction.svg'
+document.querySelector("img").src = constructionImage;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+}
